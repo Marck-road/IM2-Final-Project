16503697fd11630c8797d6026d3790e58adc1f63
Marck-road <109259291+Marck-road@users.noreply.github.com>
2023-12-04 22:45:09 +0800

    FFA Viewing

--- php/index.php
+++ php/index.php
@@ -5,9 +5,26 @@
     mysqli_select_db($con, 'loanapp');
 
     // $sort = "ASC";
-    $s = " SELECT * FROM lender ";
-    $result = mysqli_query($con, $s); 
+    if ($_SERVER["REQUEST_METHOD"] == "POST"){
+        $amt_borrowed = $_POST["amt_borrowed"];
+        $interest_rate = $_POST["interest"];
+        $tenure = $_POST["tenure"];
+        $pay_schedule = $_POST["pay_sched"];
+    } else{
+        $amt_borrowed = 200000;
+        $interest_rate = "";
+        $tenure = 4;
+        $pay_schedule = "Monthly";
+    }
     
+    $s = "SELECT DISTINCT * 
+      FROM lender 
+      RIGHT JOIN lender_interest_rates ON lender.Lender_ID = lender_interest_rates.Lender_ID
+      WHERE lender_interest_rates.Tenure_ID = $tenure
+        AND $amt_borrowed >= MinLoan_Amt
+        AND $amt_borrowed <= MaxLoan_Amt";
+
+    $result = mysqli_query($con, $s);
 ?>
 
 <html lang="en">
@@ -19,16 +36,12 @@
     <link rel="stylesheet" href="../css/navbar.css">
     <link rel="stylesheet" href="../css/slideshow.css">
     <link rel="stylesheet" href="../css/searchBar.css">
+    <link rel="stylesheet" href="../css/index.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400&display=swap" rel="stylesheet">
 </head>
 <body>
-    <label class="hamburger_menu">
-        <input type="checkbox">
-    </label>
-
-
     <nav>
         <div class="navbar">
             <div class="logo">
@@ -67,34 +80,139 @@
         
     </div>
 
-    <div class= "SearchView">
-        <p class="adv_label">Advanced Search</p>
-        <form>
+    <div class="main-container">
+        <div class= "SearchView">
+            <p class="adv_label">Advanced Search</p>
+            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
 
-            <label for="amt_borrowed">Amount Borrowed</label>
-            <input type="number" name="amt_borrowed" id="amt_borrowed" value="200000"><br>
-            <label for="interest">Interest Rate (per month)</label>
-            <input type="number" name="interest" id="interest"><br>
-            <label for="tenure">Tenure:</label>
-            <select name="tenure" id="tenure">
-                <option value="1 Month">1 Month</option>
-                <option value="3 Months">3 Months</option>
-                <option value="6 Months">6 Months</option>
-                <option value="1 Year" selected>1 Year</option>
-                <option value="2 Years">2 Years</option>
-            </select> 
-            <label for="pay_sched">Payment Schedule:</label>
-            <select name="pay_sched" id="pay_sched">
-                <option value="Weekly">Weekly</option>
-                <option value="Semi-Monthly">Semi-Monthly</option>
-                <option value="Monthly" selected>Monthly</option>
-                <option value="Quarterly">Quarterly</option>
-            </select> <br>
-            <div class="button-container">
-                <input type="submit" value="Submit"> 
+                <label for="amt_borrowed">Amount Borrowed</label>
+                <input type="number" name="amt_borrowed" id="amt_borrowed" value="<?php echo isset($_POST['amt_borrowed']) ? $_POST['amt_borrowed'] : '200000'; ?>"><br>
+
+                <label for="interest">Interest Rate (per month)</label>
+                <input type="number" name="interest" id="interest" value="<?php echo isset($_POST['interest']) ? $_POST['interest'] : ''; ?>"><br>
+
+                <label for="tenure">Tenure:</label>
+                <select name="tenure" id="tenure">
+                    <option value="1" <?php echo (isset($_POST['tenure']) && $_POST['tenure'] == '1') ? 'selected' : ''; ?>>1 Month</option>
+                    <option value="2" <?php echo (isset($_POST['tenure']) && $_POST['tenure'] == '2') ? 'selected' : ''; ?>>3 Months</option>
+                    <option value="3" <?php echo (isset($_POST['tenure']) && $_POST['tenure'] == '3') ? 'selected' : ''; ?>>6 Months</option>
+                    <option value="4" <?php echo (isset($_POST['tenure']) && $_POST['tenure'] == '4') ? 'selected' : ''; ?>>1 Year</option>
+                    <option value="5" <?php echo (isset($_POST['tenure']) && $_POST['tenure'] == '5') ? 'selected' : ''; ?>>2 Years</option>
+                </select>
+
+                <label for="pay_sched">Payment Schedule:</label>
+                <select name="pay_sched" id="pay_sched">
+                    <option value="1" <?php echo (isset($_POST['pay_sched']) && $_POST['pay_sched'] == '1') ? 'selected' : ''; ?>>Weekly</option>
+                    <option value="2" <?php echo (isset($_POST['pay_sched']) && $_POST['pay_sched'] == '2') ? 'selected' : ''; ?>>Semi-Monthly</option>
+                    <option value="3" <?php echo (isset($_POST['pay_sched']) && $_POST['pay_sched'] == '3') ? 'selected' : ''; ?>>Monthly</option>
+                    <option value="4" <?php echo (isset($_POST['pay_sched']) && $_POST['pay_sched'] == '4') ? 'selected' : ''; ?>>Quarterly</option>
+                </select>
+                      <br>
+                    <div class="button-container">
+                        <input type="submit" value="Submit"> 
+                    </div>
+            </form>
+        </div>
+
+        <div class="ListView">
+            <div class="List_header">
+                <h1>Available Loans</h1>
             </div>
-        </form>
+            <?php
+            while($row = mysqli_fetch_array($result))
+            {
+             $lender_ir = "SELECT Interest_Rate FROM lender_interest_rates
+              WHERE Lender_ID = '" . $row["Lender_ID"] . "' AND Tenure_ID = '" . $tenure . "'";
+            
+            $ir_query = mysqli_query($con, $lender_ir);
+            $ir_result = mysqli_fetch_array($ir_query);
+            $interest_payable = ($amt_borrowed * $ir_result["Interest_Rate"]);
+            
+            $ir_result["Interest_Rate"] = 100 * $ir_result["Interest_Rate"];
+            $total_payable = $interest_payable + $amt_borrowed;
+
+            if($tenure == 1){
+                $monthly_interest = $ir_result["Interest_Rate"];
+                $monthly_payable = number_format($total_payable, 2, '.', ',');
+            } else if ($tenure == 2){
+                $monthly_interest = round($ir_result["Interest_Rate"] / 2, 2);
+                $monthly_payable = number_format($total_payable / 2, 2, '.', ',');
+            } else if ($tenure == 3){
+                $monthly_interest = round($ir_result["Interest_Rate"] / 6, 2);
+                $monthly_payable = number_format($total_payable / 6, 2, '.', ',');
+            } else if ($tenure == 4){
+                $monthly_interest = round($ir_result["Interest_Rate"] / 12, 2);
+                $monthly_payable= number_format($total_payable / 12, 2, '.', ',');
+            } else if ($tenure == 5){
+                $monthly_interest = round($ir_result["Interest_Rate"] / 24, 2);
+                $monthly_payable= number_format($total_payable / 24, 2, '.', ',');
+            } 
+            
+                // $lender_ir = $lender_ir * 100;
+
+                
+            ?>
+                <div class="loan_container">
+                <!-- Column 1: Small Image -->
+                <div class="centered_column">
+                    <img src="../images/slideshow2.png" alt="Company Logo" class="company_logo">
+                </div>
+
+                <!-- Column 2: Company Name, Description -->
+                <div class="column">
+                    <div class="row">
+                        <h2><?php echo $row['Lender_Name'];?></h2>
+                    </div>
+                    <div class="row">
+                        <p><?php echo $row['Description'];?></p>
+                    </div>
+                </div>
+
+                <!-- Column 3: Interest Rate -->
+                <div class="column">
+                    <div class="row">
+                        <h3>Interest Rate</h3>
+                    </div>
+                    <div class="row_value">
+                        <p><?php echo $monthly_interest;?>%</p>
+                    </div>
+                    <div class="row">
+                        <h3>per month</h3>
+                    </div>
+                </div>
+
+                <!-- Column 4: Monthly Repayment -->
+                <div class="column">
+                    <div class="row">
+                        <h3>Monthly Repayment</h3>
+                    </div>
+                    <div class="row_value">
+                        <p>₱<?php echo $monthly_payable;?></p>
+                    </div>
+                </div>
+
+                <!-- Column 5: Apply Now Button, More Info Button -->
+                <div class="centered_column">
+                    <div class="row">
+                        <button>Apply Now</button>
+                    </div>
+                    <div class="row">
+                        <button>More Info</button>
+                    </div>
+                </div>
+            </div>
+
+            <?php
+            }
+            ?>
+        </div>
+
     </div>
+
+    <script>
+
+    </script>
+
 
     <script src="../js/slideshow.js"></script>
 </body>
