2dd11388932e58e05e683f6aca8e6ec4aa342a27
athmaw <146990004+athmaw@users.noreply.github.com>
2023-12-08 22:30:20 +0800

    Add files via upload

---
+++ php/userProfile.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>User Profile</title>
+        <link rel="stylesheet" href="../css/navbar.css">
+        <link rel="stylesheet" href="../css/userProfile.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-..."
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="../css/loginpage.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400&display=swap" rel="stylesheet">
+    </head>
+
+    <body>
+        <nav>
+            <div class="navbar">
+                <div class="logo">
+                    <a href="../php/index.php"><img src="../images/ldaddy.png" class="logo"></a>
+                </div>
+                <div class="navbar-center">
+                    <h1>USER PROFILE</h1>
+                </div>
+                <ul class="menu">
+                </ul>
+            </div>
+        </nav>
+        
+        <div class="user-profile">
+            <img src="../images/profile.png" alt="User Profile Image" class="profile-image">
+
+            <div class="profile-details">
+                <p class="profile-info">Name: John Doe</p>
+                <p class="profile-info">Email: johndoe@example.com</p>
+                <p class="profile-info">Phone Number: +1234567890</p>
+                <p class="profile-info">Gender: Male</p>
+                <p class="profile-info">Date of Birth: January 1, 1990</p>
+                <button class="approve-btn">Approve</button>
+                <button class="deny-btn">Deny</button>
+            </div>
+        </div>
+
+
+    </body>
+</html>
