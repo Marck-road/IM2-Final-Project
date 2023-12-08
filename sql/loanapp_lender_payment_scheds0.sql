89c8cade9901e5c832730f0c8d9a0470d3f78e5c
Marck-road <109259291+Marck-road@users.noreply.github.com>
2023-12-08 10:39:57 +0800

    Database Update (Probably if not Final Update)

---
+++ sql/loanapp_lender_payment_scheds0.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: loanapp
+-- ------------------------------------------------------
+-- Server version	8.0.34
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `lender_payment_scheds`
+--
+
+DROP TABLE IF EXISTS `lender_payment_scheds`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `lender_payment_scheds` (
+  `LenderPS_ID` int NOT NULL AUTO_INCREMENT,
+  `Schedule_ID` int DEFAULT NULL,
+  `Lender_ID` int DEFAULT NULL,
+  PRIMARY KEY (`LenderPS_ID`),
+  KEY `Schedule_ID_idx` (`Schedule_ID`),
+  KEY `Lender_ID_idx` (`Lender_ID`),
+  CONSTRAINT `Lender_ID` FOREIGN KEY (`Lender_ID`) REFERENCES `lender` (`Lender_ID`),
+  CONSTRAINT `Schedule_ID` FOREIGN KEY (`Schedule_ID`) REFERENCES `payment_sched` (`Schedule_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `lender_payment_scheds`
+--
+
+LOCK TABLES `lender_payment_scheds` WRITE;
+/*!40000 ALTER TABLE `lender_payment_scheds` DISABLE KEYS */;
+INSERT INTO `lender_payment_scheds` VALUES (1,1,1),(2,2,1),(3,3,1),(4,4,1),(5,2,2),(6,3,2),(7,1,3),(8,2,3),(9,3,3),(10,3,4),(11,2,5),(12,3,5),(13,2,6),(14,3,6),(15,2,7),(16,4,6),(17,1,8),(18,2,8),(19,3,8),(20,4,8),(21,3,7);
+/*!40000 ALTER TABLE `lender_payment_scheds` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-12-08 10:39:21
---
+++ sql/loanapp_user0.sql
@@ -0,0 +1,69 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: loanapp
+-- ------------------------------------------------------
+-- Server version	8.0.34
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `User_ID` int NOT NULL AUTO_INCREMENT,
+  `Email` varchar(255) NOT NULL,
+  `Password` varchar(255) NOT NULL,
+  `First_Name` varchar(255) NOT NULL,
+  `Middle_Name` varchar(255) DEFAULT NULL,
+  `Last_Name` varchar(255) NOT NULL,
+  `Monthly_Income` int NOT NULL,
+  `Birthday` date NOT NULL,
+  `City` varchar(45) NOT NULL,
+  `Province` varchar(45) NOT NULL,
+  `ZIP_Code` int NOT NULL,
+  `Contact_Number` varchar(45) NOT NULL,
+  `Employment_Status` enum('Employed','Unemployed','Self-Employed') NOT NULL,
+  `Income_Document` blob,
+  `ValidID_1` blob,
+  `ValidID_2` blob,
+  `Utility_Bill` blob,
+  `Created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `Verified_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`User_ID`),
+  UNIQUE KEY `User_ID_UNIQUE` (`User_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'asdf@gmail.com','912ec803b2ce49e4a541068d495ab570','asdf','adsf','asdf',2321321,'2222-02-22','asdf','afdsfa',21313,'213213','Employed',NULL,NULL,NULL,NULL,'2023-12-06 02:33:16',NULL),(2,'fdsa@email.com','fc2baa1a20b4d5190b122b383d7449fd','fdsa','fdsa','fdsa',232312,'1122-12-12','dsfsadf','fsdafsdaf',223,'32323','Unemployed',NULL,NULL,NULL,NULL,'2023-12-06 02:36:49',NULL),(3,'user1@gmail.com','81dc9bdb52d04dc20036dbd8313ed055','User1','is','Auser',40000,'1998-06-04','Cebu City','Cebu',6000,'09472659123','Employed',NULL,NULL,NULL,NULL,'2023-12-06 12:49:01',NULL);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-12-08 10:39:21
